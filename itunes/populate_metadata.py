a76b4d4d85fe3a8740fff59b07c148e0416fe206
Lam Ho <lam@senspark.com>
2017-04-04 10:25:57 +0700

    Submit for ver 1.0.6 iOS

--- itunes/populate_metadata.py
+++ itunes/populate_metadata.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = "python ../../../store-listing-toolkit/populate-v3.py metadata -platform iOS -prj-path . -sheet 1XQA-l4YzgxYOJsk1ai1T0s8V__Fi4hAImzDQIUENoh8 -customized-metadata-path ../src/itunes/metadata"
+cmd = "python ../../store-listing-toolkit/populate-v3.py metadata -platform iOS -prj-path . -sheet 1XQA-l4YzgxYOJsk1ai1T0s8V__Fi4hAImzDQIUENoh8 -customized-metadata-path ../src/itunes/metadata"
 print cmd
 os.system(cmd)
