3ce73710620704dba2f87fbb0dfdaeaa8eef44ed
Lam Ho <lam@senspark.com>
2017-04-07 13:39:11 +0700

    Update for resubmit iOS 1.0.6

---
+++ itunes/populate_metadata-lam.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os, os.path
+
+cmd = "python ../../store-listing-toolkit/populate-v3.py metadata -platform iOS -prj-path . -sheet 1XQA-l4YzgxYOJsk1ai1T0s8V__Fi4hAImzDQIUENoh8 -customized-metadata-path ../src/itunes/metadata"
+print cmd
+os.system(cmd)
